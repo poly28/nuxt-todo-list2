95083562a54afa82c8894863218efe38d8abdab3
poly28 <horimasa0328@gmail.com>
2021-06-29 07:23:57 +0900

    タスク名、詳細の編集機能追加

--- components/todos.vue
+++ components/todos.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-app>
-      <v-app-bar dense app color="green">
+      <v-app-bar dense app color="primary" class="white--text">
         <v-app-bar-title class="ml-5">
           Todoリスト
         </v-app-bar-title>
@@ -25,35 +25,11 @@
             </v-row>
           </v-container>
         </v-form>
-        <v-alert v-show="!input.check" type="error"
+        <v-alert v-if="!input.check" type="error"
           >タスクを入力してください！</v-alert
         >
-        <!-- <v-divider></v-divider> -->
-
         <section>
           <!-- todo -->
-          <!-- expantion panelの高度なカスタマイズ実装-->
-          <!-- https://vuetifyjs.com/ja/components/expansion-panels/#section-9ad85ea6306a4f7f304465b9 -->
-          <!-- <v-expansion-panels>
-            <v-expansion-panel
-              v-for="(todo, index) in filteredList"
-              :key="todo.id"
-            >
-              <v-expansion-panel-header>
-                <span :class="{ done: todo.isDone }">
-                  <input type="checkbox" v-model="todo.isDone" />
-                  <template>
-                    {{ todo.task }}
-                  </template>
-                  <v-btn icon="mdi-plus-thick"></v-btn>
-                  <button @click="deleteTask(index)">
-                    削除
-                  </button>
-                </span>
-              </v-expansion-panel-header>
-              <v-expansion-panel-content> - </v-expansion-panel-content>
-            </v-expansion-panel>
-          </v-expansion-panels> -->
           <!-- <ul>
             <li v-for="(todo, index) in filteredList" :key="todo.id">
               <span :class="{ done: todo.isDone }">
@@ -70,7 +46,77 @@
             :items-per-page="10"
             class="elevation-1"
           >
-            <template></template>
+            <!-- 完了、未完了チェックボックス切り替え機能ここから -->
+            <template v-slot:[`item.check`]="{ item }">
+              <v-simple-checkbox v-model="item.isDone"></v-simple-checkbox>
+            </template>
+            <!-- 完了、未完了チェックボックス切り替え機能ここまで -->
+
+            <!-- タスク名編集機能ここから -->
+            <template v-slot:[`item.task`]="props">
+              <v-edit-dialog
+                :return-value.sync="props.item.task"
+                large
+                persistent
+                @save="save"
+                @cancel="cancel"
+                @open="open"
+                @close="close"
+              >
+                <div>{{ props.item.task }}</div>
+                <template v-slot:input>
+                  <div class="mt-4 text-h6">
+                    Update Iron
+                  </div>
+                  <v-text-field
+                    v-model="props.item.task"
+                    :rules="[max25chars]"
+                    label="Edit"
+                    single-line
+                    counter
+                    autofocus
+                  ></v-text-field>
+                </template>
+              </v-edit-dialog>
+            </template>
+            <!-- タスク名編集機能ここまで -->
+
+            <!-- 詳細内容編集機能ここから -->
+            <template v-slot:[`item.detail`]="props">
+              <v-edit-dialog
+                :return-value.sync="props.item.detail"
+                large
+                persistent
+                @save="save"
+                @cancel="cancel"
+                @open="open"
+                @close="close"
+              >
+                <div>{{ props.item.detail }}</div>
+                <template v-slot:input>
+                  <div class="mt-4 text-h6">
+                    Update Iron
+                  </div>
+                  <v-text-field
+                    v-model="props.item.detail"
+                    :rules="[max25chars]"
+                    label="Edit"
+                    single-line
+                    counter
+                    autofocus
+                  ></v-text-field>
+                </template>
+              </v-edit-dialog>
+            </template>
+            <!-- 詳細内容編集機能ここまで -->
+
+            <!-- タスク削除機能ここから -->
+            <template v-slot:[`item.actions`]="{ item }">
+              <v-icon small @click="deleteTask(item)">
+                mdi-delete
+              </v-icon>
+            </template>
+            <!-- タスク削除機能ここまで -->
           </v-data-table>
         </section>
         <!-- デバッグ -->
@@ -89,12 +135,13 @@
 
 // ▼ほしい機能
 // ■TODOの追加・削除
-// □TODOの編集機能
-// □TODOのタイトル編集機能
+// ■TODOの編集機能
+// ■TODOのタイトル編集機能
 // ■TODOのステータス（完了／未完了）の切替機能
 
 // 【余裕があればやってみてください】
 // ▼ソート、フィルター、期限設定の機能など自分で思いついた機能を追加する。
+// □タスクの完了状態に合わせてデザインを適用する
 
 export default {
   data: function() {
@@ -104,7 +151,8 @@ export default {
         { text: "check", value: "check" },
         { text: "id", value: "id" },
         { text: "task", value: "task" },
-        { text: "detail", value: "detail" }
+        { text: "detail", value: "detail" },
+        { text: "actions", value: "actions" }
       ],
       input: {
         text: "",
@@ -126,7 +174,8 @@ export default {
       let newTask = {
         isDone: false,
         id: this.todos.length + 1,
-        task: this.input.text
+        task: this.input.text,
+        detail: "-"
       };
 
       // タスクの追加
@@ -141,32 +190,31 @@ export default {
   },
   computed: {
     filteredList() {
-      let idTodos = this.todos;
-
       // 【自動処理：ID再振り分け】
       // タスクを削除した際に、todo配列のidを再度振り分ける
-      for (let i = 0; i < idTodos.length; i++) {
-        idTodos[i].id = i + 1;
-      }
+      // for (let i = 0; i < this.todos.length; i++) {
+      //   this.todos[i].id = i + 1;
+      // }
+      // let sortedTodos = this.todos;
 
       // 【自動処理：未完了、完了の自動ソート】
       // 未完了タスクを配列に格納する
-      // let sortTodos = [];
-      // for (let i = 0; i < idTodos.length; i++) {
-      //   if (idTodos[i].isDone === false) {
-      //     sortTodos.push(idTodos[i]);
+      // let sortedTodos = [];
+      // for (let i = 0; i < this.todos.length; i++) {
+      //   if (this.todos[i].isDone === false) {
+      //     sortTodos.push(this.todos[i]);
       //   }
       // }
 
       // 完了タスクを配列に格納する
-      // for (let i = 0; i < idTodos.length; i++) {
-      //   if (idTodos[i].isDone === true) {
-      //     sortTodos.push(idTodos[i]);
+      // for (let i = 0; i < this.todos.length; i++) {
+      //   if (this.todos[i].isDone === true) {
+      //     sortTodos.push(this.todos[i]);
       //   }
       // }
 
       // 最終的なtodo配列を返却する
-      return idTodos;
+      return this.todos;
     }
   }
 };
